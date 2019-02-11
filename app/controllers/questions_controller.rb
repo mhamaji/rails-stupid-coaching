d220edd920a14d7154150cea0785e1e57eb64b3d
Mauricio Homma <mhhomma@gmail.com>
2019-02-11 12:33:17 -0200

    Added noice style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    if params[:question].downcase.casecmp('i am going to work').zero?
+      @answer = 'Great!'
+    elsif params[:question].last == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'questions/ask'
   get 'questions/answer'
   get 'answer', to: 'questions#answer'
+  get root to: 'questions#ask'
+  get 'ask', to: 'questions#ask'
 end
