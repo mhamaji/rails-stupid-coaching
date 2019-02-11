be7f7676a28b90644f1ba61c880ea3a24889b927
Mauricio Homma <mhhomma@gmail.com>
2019-02-11 12:11:33 -0200

    Working coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'questions/ask'
+  get 'questions/answer'
+  get 'answer', to: 'questions#answer'
 end
